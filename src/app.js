5407a1e5ad7a5396b674de4e98088c140265ecb8
akash-saha-1 <aakashsaha5thjuly@gmail.com>
2021-09-20 05:40:40 +0530

    docker added

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ app.use(morgan('tiny'));
 const port = process.env.PORT || 400;
 
 //body json/url encoded parser
-var urlEncodedParser = bodyparser.urlencoded({extended : false})
+var urlEncodedParser = bodyparser.urlencoded({ extended: false });
 
 // for html template
 const staicPath = path.join(__dirname, '../public');
@@ -27,58 +27,58 @@ app.use(express.static(staicPath));
 app.set('view engine', 'hbs');
 
 //home page
-app.get('/', async(req, res)=>{
-    try{
-        let countries = await countriesList.find();
-        res.render('index', {'countriesList': countries});
-    }catch(err){
-        console.error(err);
-        res.status(500).render('error');
-    }
-})
+app.get('/', async (req, res) => {
+  try {
+    let countries = await countriesList.find();
+    res.render('index', { countriesList: countries });
+  } catch (err) {
+    console.error(err);
+    res.status(500).render('error');
+  }
+});
 
 //submit page, create new country in our database
-app.post('/country', urlEncodedParser, async (req,res)=>{
-    try{
-        //console.log(req.body);
-        const newAddedCountry = new addedCountries({
-            id: req.body.id,
-            code: req.body.code,
-            name: req.body.name,
-            nativeName: req.body.nativeName,
-            class: req.body.class
-        })
-        let name = req.body.name;
-        if(!name || name==""){
-            console.error('invalid request');
-            return res.redirect('/');
-        }
-        await newAddedCountry.save();
-        req.body.name = '';
-        return res.status(201).render('success', {data: name});
-    }catch(err){
-        console.error(err);
-        return res.status(500).send({'error':'something went wrong!'})
+app.post('/country', urlEncodedParser, async (req, res) => {
+  try {
+    //console.log(req.body);
+    const newAddedCountry = new addedCountries({
+      id: req.body.id,
+      code: req.body.code,
+      name: req.body.name,
+      nativeName: req.body.nativeName,
+      class: req.body.class,
+    });
+    let name = req.body.name;
+    if (!name || name == '') {
+      console.error('invalid request');
+      return res.redirect('/');
     }
-})
+    await newAddedCountry.save();
+    req.body.name = '';
+    return res.status(201).render('success', { data: name });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).send({ error: 'something went wrong!' });
+  }
+});
 
 //intial json data
-app.get('/countriesList', async(req, res) =>{
-    try{
-        let countries = await countriesList.find();
-        res.send(countries);
-    }catch(err){
-        console.error(err);
-        res.status(500).send('something went wrong');
-    }
+app.get('/countriesList', async (req, res) => {
+  try {
+    let countries = await countriesList.find();
+    res.send(countries);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('something went wrong');
+  }
 });
 
 // initializing database connection
 connection();
 
 //initializing server port
-app.listen(port, ()=>{
-    //console.log(`server is running on port : ${port}`);
+app.listen(port, () => {
+  //console.log(`server is running on port : ${port}`);
 });
 
 module.exports = app;
--- src/database/connection.js
+++ src/database/connection.js
@@ -1,39 +1,55 @@
 const mongoose = require('mongoose');
 const Mockgoose = require('mockgoose').Mockgoose;
-const countriesListJson = require('./data.json')
+const countriesListJson = require('./data.json');
 
 const port = process.env.PORT || 400;
 
-const connection = async()=>{
-    if(process.env.NODE_ENV == 'test'){
-        //setting mock database for unit testing
-        const mockgoose = new Mockgoose(mongoose);
-        mockgoose.prepareStorage().then(() => 
-        mongoose.connect('mongodb+srv://akash:12345@cluster0.hf7eb.mongodb.net/unit-testing?retryWrites=true&w=majority',{
+const connection = async () => {
+  if (process.env.NODE_ENV == 'test') {
+    //setting mock database for unit testing
+    const mockgoose = new Mockgoose(mongoose);
+    mockgoose.prepareStorage().then(() =>
+      mongoose
+        .connect(
+          'mongodb+srv://akash:12345@cluster0.hf7eb.mongodb.net/unit-testing?retryWrites=true&w=majority',
+          {
             useNewUrlparser: true,
-            useUnifiedTopology: true
-        }).then((db)=>{
-            console.log('sucessfully connected to test database');
-        }).catch((err)=> console.error('Error while connecting to db due to : ' + err)));
-
-    }else{
-        mongoose.connect('mongodb+srv://akash:12345@cluster0.hf7eb.mongodb.net/employee?retryWrites=true&w=majority',{
-            useNewUrlparser: true,
-            useUnifiedTopology: true
-        }).then((db)=>{
-            // first time collection countrieslist setup
-            // db.connection.collection('countrieslists').insertMany(countriesListJson, (error, inserted)=>{
-            //     if(error) {
-            //         console.error(error);
-            //     }
-            //     else {
-            //         console.log("Successfully inserted: " , inserted );
-            //     }
-            // })
+            useUnifiedTopology: true,
+          }
+        )
+        .then((db) => {
+          console.log('sucessfully connected to test database');
+        })
+        .catch((err) =>
+          console.error('Error while connecting to db due to : ' + err)
+        )
+    );
+  } else {
+    mongoose
+      .connect(
+        'mongodb+srv://akash:12345@cluster0.hf7eb.mongodb.net/employee?retryWrites=true&w=majority',
+        {
+          useNewUrlparser: true,
+          useUnifiedTopology: true,
+        }
+      )
+      .then((db) => {
+        // first time collection countrieslist setup
+        // db.connection.collection('countrieslists').insertMany(countriesListJson, (error, inserted)=>{
+        //     if(error) {
+        //         console.error(error);
+        //     }
+        //     else {
+        //         console.log("Successfully inserted: " , inserted );
+        //     }
+        // })
         // console.log('sucessfully connected to database');
         console.log(`server is running on port : ${port}`);
-        }).catch((err)=> console.error('Error while connecting to db due to : ' + err));
-    }
-}
+      })
+      .catch((err) =>
+        console.error('Error while connecting to db due to : ' + err)
+      );
+  }
+};
 
 module.exports = connection;
